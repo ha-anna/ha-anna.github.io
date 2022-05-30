c00a25a2158efa14441286d57dbe697cc03e4d15
Ha Anna <its.haanna@gmail.com>
2022-05-30 13:07:26 +0900

    ADD: Personal Dashboard project

---
+++ Scrimba_Projects/Personal_dashboard/README.md
@@ -0,0 +1,52 @@
+# Personal Dashboard [30 May 2022]
+
+This project is a part of Scrimba's Frontend Developer Career Path.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### Screenshot
+
+![alt text]()
+
+### Links
+
+- Live Site URL: []()
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- SASS
+- Mobile-first workflow
+- Vanilla JavaScript
+- API (Geolocation, Cryptocurrency, Unsplash)
+
+### What I learned
+
+### Continued development
+
+### Useful resources
+
+- [Scrimba](https://www.scrimba.com)
+
+## Author
+
+- Website - [Ha Anna](https://haanna.com)
+
+## Acknowledgments
+
+Thank you, Scrimba #scrimba-pets channel for the inspiration, as well as other Scrimba's students for their reviews and opinions.
---
+++ Scrimba_Projects/Personal_dashboard/script.js
@@ -0,0 +1,100 @@
+const cryptoName = 'bitcoin'
+const photoGenre = 'cat'
+const timeStyle = 'medium'
+const fallbackImg = 'https://images.unsplash.com/photo-1491036775913-3fbc5c455842?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTM4NDAyNTk&ixlib=rb-1.2.1&q=80&w=1080'
+const units = 'metric'
+
+// 1. getting wallpaper from unsplash
+
+fetch(`https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=${photoGenre}`)
+    .then(res => {
+    if (!res.ok) {
+      throw Error("An issue with API occurred.")
+    }
+    console.log(res.status)
+    return res.json()
+  })
+    .then(data => {
+        document.body.style.backgroundImage = `url(${data.urls.full})`
+		    document.getElementById("author").innerHTML = `<p>By: <a href="${data.user.links.html}" target="_blank">${data.user.name}</a></p>`
+    }).catch(err => {
+      document.body.style.backgroundImage = `url(${fallbackImg})`
+    })
+
+// 2. getting dogecoin info
+
+fetch(`https://api.coingecko.com/api/v3/coins/${cryptoName}`)
+  .then(res => {
+    if (!res.ok) {
+      throw Error("An issue with API occured.")
+    }
+    console.log(res.status)
+    return res.json()
+  })
+  .then(data => {
+    document.getElementById('crypto-top').innerHTML = `
+          <img src="${data.image.small}" class="crypto-icon" alt="">
+          <h2>
+            ${data.name}
+          </h2>`
+    
+    document.getElementById('crypto-data').innerHTML = `
+      <p><b>Current:</b> $${data.market_data.current_price.usd}</p>
+      <p><b>High:</b> $${data.market_data.high_24h.usd}</p>
+      <p><b>Low:</b> $${data.market_data.low_24h.usd}</p>
+    `
+    console.log(data)
+    
+  })
+  .catch(err => console.error(err))
+
+// 3. getting current time
+
+function getCurrentTime() {
+const date = new Date()
+const formattedDate = date.toLocaleTimeString("en-us", {timeStyle: `${timeStyle}`})
+document.getElementById('time').innerHTML = `<h1>${formattedDate} </h1>`
+}
+
+setInterval(getCurrentTime, 1000)
+
+// 4. getting geolocation
+
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+
+navigator.geolocation.getCurrentPosition(position => {
+  console.log(position)
+  const lat = position.coords.latitude
+  const lon = position.coords.longitude
+
+  fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}`)
+    .then(res => {
+      if (!res.ok) {
+        throw Error("Weather data not available")
+      }
+      return res.json()
+    })
+    .then(data => {
+      console.log(data)
+      const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+
+      document.getElementById('weather').innerHTML = `
+      <div class="weather-top">
+        <h2>Weather</h2>
+        <img src="${iconUrl}" alt="" class="weather-icon">
+      </div>
+      <p><b>Location:</b> ${data.name}</p>
+      <p><b>Temp:</b> ${data.main.temp}°C</p>
+      <p><b>Feels like:</b> ${data.main.feels_like}°C</p>
+      <p><b>Sky:</b> ${data.weather[0].main}</p>
+      `
+    })
+    .catch(err => console.error(err))
+
+});
+
