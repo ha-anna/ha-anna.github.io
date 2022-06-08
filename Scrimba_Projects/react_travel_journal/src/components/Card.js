39f5942192c6f742f0c41d77cb0f4bd569e4e4eb
Ha Anna <its.haanna@gmail.com>
2022-06-08 13:45:32 +0900

    FIX: CSS

--- Scrimba_Projects/react_travel_journal/src/components/Card.js
+++ Scrimba_Projects/react_travel_journal/src/components/Card.js
@@ -13,7 +13,7 @@ export default function Card(props) {
                 <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
               </svg>
               {props.location}</span>
-            <a href={props.links.googleMaps} target="_blank" className="link">View on Google Maps</a>
+            <button href={props.links.googleMaps} target="_blank" className="link">View on Google Maps</button>
           </div>
           <h2>{props.title}</h2>
           <span className="bold small">{props.dates.start} - {props.dates.end}</span>
