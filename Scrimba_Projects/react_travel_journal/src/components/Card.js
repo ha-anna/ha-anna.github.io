211a808294ebd7582d91b5a3d7f32c43ac2c0402
Ha Anna <its.haanna@gmail.com>
2022-06-08 13:59:57 +0900

    FIX: LINK

--- Scrimba_Projects/react_travel_journal/src/components/Card.js
+++ Scrimba_Projects/react_travel_journal/src/components/Card.js
@@ -13,7 +13,7 @@ export default function Card(props) {
                 <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
               </svg>
               {props.location}</span>
-            <button href={props.links.googleMaps} target="_blank" className="link">View on Google Maps</button>
+            <a href={props.links.googleMaps} className="link"><button type="button">View on Google Maps</button></a>
           </div>
           <h2>{props.title}</h2>
           <span className="bold small">{props.dates.start} - {props.dates.end}</span>
