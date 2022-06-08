0d8b32601493d62ad0bd214d75a22a27bffbe2fa
Ha Anna <its.haanna@gmail.com>
2022-06-09 00:09:25 +0900

    UPLOAD: jpg changed to webp

--- Scrimba_Projects/react_travel_journal/src/data.js
+++ Scrimba_Projects/react_travel_journal/src/data.js
@@ -4,7 +4,7 @@ export default [
     location: "Vietnam",
     title: "Ho Chi Minh City",
     description: "Ho Chi Minh City, formerly known as Saigon, is the largest city in Vietnam, situated in the south. In the southeastern region, the city surrounds the Saigon River and covers about 2,061 km².",
-    image: "hochiminh2.jpg",
+    image: "hochiminh2.webp",
     dates: {
       start: "4 Sept 2019",
       end: "9 Sept 2019"
@@ -18,7 +18,7 @@ export default [
     location: "Poland",
     title: "Bieszczady Mountains",
     description: "Bieszczady Mountains is a mountain range that runs from the extreme south-east of Poland and north-east of Slovakia through to western Ukraine. It forms the western part of the Eastern Beskids, and is more generally part of the Outer Eastern Carpathians.",
-    image: "bieszczady.jpg",
+    image: "bieszczady.webp",
     dates: {
       start: "1 Jul 2019",
       end: "14 Jul 2019"
@@ -32,7 +32,7 @@ export default [
     location: "South Korea",
     title: "Seoul City",
     description: "Seoul, the capital of South Korea, is a huge metropolis where modern skyscrapers, high-tech subways and pop culture meet Buddhist temples, palaces and street markets. Notable attractions include futuristic Dongdaemun Design Plaza, a convention hall with curving architecture and a rooftop park; Gyeongbokgung Palace, which once had more than 7,000 rooms; and Jogyesa Temple, site of ancient locust and pine trees.",
-    image: "seoul.jpg",
+    image: "seoul.webp",
     dates: {
       start: "17 Jul 2019",
       end: "23 Sept 2019"
@@ -46,7 +46,7 @@ export default [
     location: "Italy",
     title: "Rome",
     description: "Rome is the capital city of Italy. It is also the capital of the Lazio region, the centre of the Metropolitan City of Rome, and a special comune named Comune di Roma Capitale.",
-    image: "rome.jpg",
+    image: "rome.webp",
     dates: {
       start: "28 Dec 2019",
       end: "2 Jan 2020"
@@ -60,7 +60,7 @@ export default [
     location: "South Korea",
     title: "Jeju-do",
     description: "Jeju is known for its beach resorts and volcanic landscape of craters and cavelike lava tubes. Hallasan Mountain, a dormant volcano, features hiking trails, a crater lake at the 1,950m summit and nearby Gwaneumsa Temple. The Geomunoreum Lava Tube System includes 7km-long Manjanggul Cave, created centuries ago when Hallasan was still active. ",
-    image: "jeju.jpg",
+    image: "jeju.webp",
     dates: {
       start: "23 Nov 2021",
       end: "26 Nov 2021"
@@ -74,7 +74,7 @@ export default [
     location: "South Korea",
     title: "Busan City",
     description: "Busan, a large port city in South Korea, is known for its beaches, mountains and temples. Busy Haeundae Beach features the Sea Life Aquarium, plus a Folk Square with traditional games such as tug-of-war, while Gwangalli Beach has many bars and views of modern Diamond Bridge. Beomeosa Temple, a Buddhist shrine founded in 678 A.D., is at the base of Geumjeong Mountain, which has hiking trails.",
-    image: "busan.jpg",
+    image: "busan.webp",
     dates: {
       start: "21 March 2022",
       end: "23 March 2022"
