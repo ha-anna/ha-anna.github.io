651417d9dd381e8f0cf4ced5b654c55f61ab4b69
Ha Anna <its.haanna@gmail.com>
2022-06-08 16:15:21 +0900

    CSS: update

--- Scrimba_Projects/react_travel_journal/src/components/Card.js
+++ Scrimba_Projects/react_travel_journal/src/components/Card.js
@@ -7,7 +7,7 @@ export default function Card(props) {
       <img src={`../images/${props.image}`} alt="" className="card--img" />
       <div className="card--data">
         <div className="card--data__location">
-          <span className="letter-spacing uppercase margin-right nowrap padding">
+          <span className="location">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-geo-alt-fill" viewBox="0 0 16 16">
               <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
             </svg>
