07e07636c1af1c55de19f597daa1ff2c42e58aef
Ha Anna <its.haanna@gmail.com>
2022-06-10 01:15:02 +0900

    UPLAD: added functions

--- Scrimba_Projects/basketball_scoreboard/index.js
+++ Scrimba_Projects/basketball_scoreboard/index.js
@@ -3,6 +3,7 @@ const $guestScore = document.getElementById('guest-score')
 const $resetBtn = document.getElementById('reset-btn')
 const $homeName = document.getElementById('home-name')
 const $guestName = document.getElementById('guest-name')
+const $alert = document.getElementById('alert')
 
 const buttonsHome = document.querySelectorAll('.button-home')
 const buttonsGuest = document.querySelectorAll('.button-guest')
@@ -14,12 +15,6 @@ buttonsHome.forEach(button => button.addEventListener('click', addScoreHome))
 buttonsGuest.forEach(button => button.addEventListener('click', addScoreGuest))
 
 function render() {
-    if (homeScore >= 21 || guestScore >= 21) {
-    $resetBtn.style.display = 'unset';
-    buttonsHome.forEach(button => button.disabled = true)
-    buttonsGuest.forEach(button => button.disabled = true)
-    }
-    
     $homeScore.textContent = homeScore
     $guestScore.textContent = guestScore
     checkLead()
@@ -37,6 +32,12 @@ function addScoreGuest(event) {
 }
 
 function checkLead() {
+    if (homeScore >= 21 || guestScore >= 21) {
+        $resetBtn.style.display = 'unset';
+        buttonsHome.forEach(button => button.disabled = true)
+        buttonsGuest.forEach(button => button.disabled = true)
+    }
+    
     if (homeScore === guestScore) {
         $homeName.textContent = 'HOME'
         $guestName.textContent = 'GUEST'
@@ -47,11 +48,20 @@ function checkLead() {
         $guestName.textContent = 'GUEST 👑'
         $homeName.textContent = 'HOME'
     }
+    
+    if(homeScore > guestScore && homeScore >= 21) {
+       $alert.textContent = `HOME TEAM WINS` 
+        $alert.style.display= 'unset'
+    } else if (guestScore > homeScore && guestScore >= 21) {
+        $alert.textContent = `GUEST TEAM WINS`
+        $alert.style.display= 'unset'
+    }
 }
 
 function reset() {
     buttonsHome.forEach(button => button.disabled = false)
     buttonsGuest.forEach(button => button.disabled = false)
+    $alert.style.display= 'none'
     homeScore = 0
     guestScore = 0
     render()
