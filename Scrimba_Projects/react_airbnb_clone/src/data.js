a4a6558d9a3b2561147833819632271fc6f0b369
Ha Anna <its.haanna@gmail.com>
2022-06-13 10:58:34 +0900

    UPDATE: added animation and transitions effects

--- Scrimba_Projects/react_airbnb_clone/src/data.js
+++ Scrimba_Projects/react_airbnb_clone/src/data.js
@@ -40,80 +40,80 @@ export default [
   },
   {
     id: 4,
-    title: "Life Lessons with Katie Zaferes",
-    description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
-    price: 136,
-    coverImg: "katie-zaferes.png",
+    title: "Pool Party",
+    description: "",
+    price: 200,
+    coverImg: "File 1.jpg",
     stats: {
-      rating: 5.0,
-      reviewCount: 6
+      rating: 4.6,
+      reviewCount: 50
     },
-    location: "Online",
-    openSpots: 0,
+    location: "Los Angeles, USA",
+    openSpots: 10,
   },
   {
     id: 5,
-    title: "Learn Wedding Photography",
-    description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
-    price: 125,
-    coverImg: "wedding-photography.png",
+    title: "Adventure Park Date",
+    description: "",
+    price: 55,
+    coverImg: "File 2.jpg",
     stats: {
-      rating: 5.0,
-      reviewCount: 30
+      rating: 4.0,
+      reviewCount: 10
     },
-    location: "Online",
-    openSpots: 27,
+    location: "New Jersey, USA",
+    openSpots: 5,
   },
   {
     id: 6,
-    title: "Group Mountain Biking",
-    description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
-    price: 50,
-    coverImg: "mountain-bike.png",
+    title: "Sand Biking",
+    description: "",
+    price: 150,
+    coverImg: "File 6.jpg",
     stats: {
-      rating: 4.8,
-      reviewCount: 2
+      rating: 3.8,
+      reviewCount: 5
     },
-    location: "Norway",
-    openSpots: 3,
+    location: "Egypt",
+    openSpots: 0,
   },
   {
     id: 7,
-    title: "Life Lessons with Katie Zaferes",
-    description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
-    price: 136,
-    coverImg: "katie-zaferes.png",
+    title: "Palace Viewing",
+    description: "",
+    price: 30,
+    coverImg: "File 3.jpg",
     stats: {
-      rating: 5.0,
-      reviewCount: 6
+      rating: 4.8,
+      reviewCount: 30
     },
-    location: "Online",
+    location: "Seoul, South Korea",
     openSpots: 0,
   },
   {
     id: 8,
-    title: "Learn Wedding Photography",
-    description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
-    price: 125,
-    coverImg: "wedding-photography.png",
+    title: "Extreme Kayaking",
+    description: "",
+    price: 178,
+    coverImg: "File 4.jpg",
     stats: {
-      rating: 5.0,
-      reviewCount: 30
+      rating: 3.0,
+      reviewCount: 1
     },
-    location: "Online",
-    openSpots: 27,
+    location: "Tatry Mountains",
+    openSpots: 13,
   },
   {
     id: 9,
-    title: "Group Mountain Biking",
-    description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+    title: "Gondola Ride",
+    description: "",
     price: 50,
-    coverImg: "mountain-bike.png",
+    coverImg: "File 5.jpg",
     stats: {
-      rating: 4.8,
-      reviewCount: 2
+      rating: 4.3,
+      reviewCount: 21
     },
-    location: "Norway",
-    openSpots: 3,
+    location: "End of the world",
+    openSpots: 5,
   }
 ]
