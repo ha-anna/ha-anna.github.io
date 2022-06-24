e2060f9c686eccf319f9f2a012b1e44d1cbbbe8e
Ha Anna <its.haanna@gmail.com>
2022-06-24 18:24:36 +0200

    FIX: image path

--- Scrimba_Projects/react_meme_generator/src/components/Header.js
+++ Scrimba_Projects/react_meme_generator/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <header>
-      <img src="./images/Icon_doge.webp" alt="" className="logo" draggable="false" />
+      <img src="./images/icon_doge.webp" alt="" className="logo" draggable="false" />
     </header>
   )
 }
