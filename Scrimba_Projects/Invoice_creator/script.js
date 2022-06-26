63b020e01b51bd8f2f0417ca77883fe446918d1c
Ha Anna <its.haanna@gmail.com>
2022-06-26 19:16:35 +0200

    UPDATE: invoice generator code refactor

--- Scrimba_Projects/Invoice_creator/script.js
+++ Scrimba_Projects/Invoice_creator/script.js
@@ -1,14 +1,12 @@
-/* eslint-disable comma-dangle */
-/* eslint-disable require-jsdoc */
 // variables
 const washBtn = document.getElementById('wash-btn');
 const mowBtn = document.getElementById('mow-btn');
 const weedBtn = document.getElementById('weed-btn');
 const sendBtn = document.getElementById('send-btn');
-const taskEl = document.getElementById('task-el');
+const invoiceTasksEl = document.getElementById('invoice-tasks');
 const totalEl = document.getElementById('total-el');
 const notesEl = document.getElementById('notes-txt');
-const amountText = document.getElementById('amount-text');
+const allTaskButtons = document.querySelectorAll('.task-button');
 let sum = 0;
 
 // array of objects holding service names and prices
@@ -27,120 +25,72 @@ const services = [
   }
 ];
 
-// .addEventListeners
+// event listeners
 
-washBtn.addEventListener('click', addWash);
-mowBtn.addEventListener('click', addMow);
-weedBtn.addEventListener('click', addWeed);
+washBtn.addEventListener('click', function () {
+  addItemToInvoice(this.value);
+  this.disabled = true;
+});
+
+mowBtn.addEventListener('click', function () {
+  addItemToInvoice(this.value);
+  this.disabled = true;
+});
+
+weedBtn.addEventListener('click', function () {
+  addItemToInvoice(this.value);
+  this.disabled = true;
+});
+
 sendBtn.addEventListener('click', reset);
 
 // functions
-function addWash() {
-  taskEl.innerHTML += ` 
-    <div class="service" id="washEl" onclick="removeWash()">
-    <span class="service-txt">
-    ${services[0].name}
-    </span>
-    <button type="button" id="remove-el">
-    <i class="bi bi-trash-fill"></i>
-     Remove
-    </button>
+
+function addItemToInvoice(value) {
+  invoiceTasksEl.innerHTML += ` 
+    <div class="service-div">
+    <div>
+      <span class="service-txt">
+        ${services[value].name}
+      </span>
+      <button type="button" id=${value} class="remove-btn">
+        <i class="bi bi-trash-fill"></i>
+        Remove
+      </button>
+    </div>
+    <span class="service-txt">$${services[value].price}</span>
     </div>
     `;
-  totalEl.innerHTML += ` 
-    <span class="service-txt" id="washPriceEl""> $${services[0].price}
-    </span>
-    `;
-  sum += services[0].price;
-  amountText.textContent = `$${sum}`;
+
+  sum += services[value].price;
+  totalEl.textContent = `$${sum}`;
   notesEl.style.opacity = '1';
-  // this stops charging > once for the same service
-  washBtn.removeEventListener('click', addWash);
+  document.querySelectorAll('.remove-btn').forEach((button) => {
+    button.addEventListener('click', removeFromInvoice);
+  });
 }
 
-// remove wash car element
-function removeWash() {
-  document.getElementById('washEl').remove();
-  document.getElementById('washPriceEl').remove();
-  sum += -10;
-  amountText.textContent = `$${sum}`;
-  washBtn.addEventListener('click', addWash);
-}
+// removing an element from the invoice
 
-function addMow() {
-  taskEl.innerHTML += `
-    <div class="service" id="mowEl" onclick="removeMow()">
-    <span class="service-txt">
-    ${services[1].name}
-    </span>
-    <button type="button" id="remove-el">
-    <i class="bi bi-trash-fill"></i> 
-    Remove
-    </button>
-    </div>
-    `;
-  totalEl.innerHTML += ` 
-    <span class="service-txt" id="mowPriceEl"> $${services[1].price}
-    </span>
-    `;
-  sum += services[1].price;
-  amountText.textContent = `$${sum}`;
-  notesEl.style.opacity = '1';
-  // this stops charging > once for the same service
-  mowBtn.removeEventListener('click', addMow);
-}
+function removeFromInvoice() {
+  this.parentNode.parentNode.innerHTML = '';
+  sum -= services[this.id].price;
+  totalEl.textContent = `$${sum}`;
 
-// remove mow lawn element
-function removeMow() {
-  document.getElementById('mowEl').remove();
-  document.getElementById('mowPriceEl').remove();
-  sum += -20;
-  amountText.textContent = `$${sum}`;
-  mowBtn.addEventListener('click', addMow);
-}
-
-function addWeed() {
-  taskEl.innerHTML += ` 
-    <div class="service" id="weedEl" onclick="removeWeed()">
-    <span class="service-txt">
-    ${services[2].name}
-    </span>
-    <button type="button" id="remove-el">
-    <i class="bi bi-trash-fill"></i>
-     Remove
-    </button>
-    </div>
-    `;
-  totalEl.innerHTML += ` 
-    <span class="service-txt" id="weedPriceEl"> $${services[2].price}
-    </span>
-    `;
-  sum += services[2].price;
-  amountText.textContent = `$${sum}`;
-  notesEl.style.opacity = '1';
-  // this stops charging > once for the same service
-  weedBtn.removeEventListener('click', addWeed);
-}
-
-// remove pull weeds element
-function removeWeed() {
-  document.getElementById('weedEl').remove();
-  document.getElementById('weedPriceEl').remove();
-  sum += -30;
-  amountText.textContent = `$${sum}`;
-  weedBtn.addEventListener('click', addWeed);
+  for (let button of allTaskButtons) {
+    if (button.value === this.id) {
+      button.disabled = false;
+    }
+  }
 }
 
 // "sending" the invoice
 function reset() {
   sum = 0;
-  taskEl.innerHTML = ``;
-  totalEl.innerHTML = `
-  <p>Total</p>
-  `;
-  amountText.textContent = `$${sum}`;
+  totalEl.textContent = `$${sum}`;
+  invoiceTasksEl.innerHTML = '';
   notesEl.style.opacity = '';
-  washBtn.addEventListener('click', addWash);
-  mowBtn.addEventListener('click', addMow);
-  weedBtn.addEventListener('click', addWeed);
+  washBtn.disabled = false;
+  mowBtn.disabled = false;
+  weedBtn.disabled = false;
 }
